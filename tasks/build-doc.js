225b7f19c5cd3ecbadb720e9a9e51293ffd2ab09
popomore <sakura9515@gmail.com>
2013-05-29 01:43:28 +0800

    support alice

--- tasks/build-doc.js
+++ tasks/build-doc.js
@@ -4,9 +4,10 @@ var runCommands = require('../lib/run-commands');
 
 module.exports = function(grunt) {
   grunt.registerTask('build-doc', 'Build document using nico', function(target) {
+    var theme = (pkg.family === 'alice') ? 'alice' : 'arale';
     var nicoConfig = path.join(
       spmrc.get('user.home'),
-      '.spm/themes/arale/nico.js'
+      '.spm/themes/' + theme + '/nico.js'
     );
 
     var done = this.async();
--- tasks/publish-doc.js
+++ tasks/publish-doc.js
@@ -5,9 +5,10 @@ var runCommands = require('../lib/run-commands');
 module.exports = function(grunt) {
   grunt.registerTask('publish-doc', 'Publish document', function(target) {
     var pkg = require(path.resolve('package.json'));
+    var theme = (pkg.family === 'alice') ? 'alice' : 'arale';
     var nicoConfig = path.join(
       spmrc.get('user.home'),
-      '.spm/themes/arale/nico.js'
+      '.spm/themes/' + theme + '/nico.js'
     );
     var server = ['jquery', 'arale', 'gallery'].indexOf(pkg.family) > -1 ? 'spmjs' : 'alipay';
 
