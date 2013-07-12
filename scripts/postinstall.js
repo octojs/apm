d4f555beb7f21dec72efcad589b8021d2d2616b2
偏右 <afc163@gmail.com>
2013-07-12 10:20:40 +0800

    Update postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -66,7 +66,7 @@ async.waterfall([
         console.log(e.message || e);
         console.log('  you need install spm to register the program');
         console.log();
-        console.log('    $npm install spm -g'.red);
+        console.log('    $ npm install spm -g'.red);
         console.log();
         console.log("  if you have installed spm, it maybe you haven't set a NODE_PATH environment variable");
         console.log();
